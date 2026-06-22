6bcbd4914ca2feaee371db2b3d414833290e7d1f
nicolas <nicolas.1991espinoza@gmail.com>
2026-06-22 18:51:35 -0400

    problema produccion contenedores

--- hotel-reservas/scripts/docker-entrypoint.sh
+++ hotel-reservas/scripts/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "[entrypoint] Aplicando migraciones..."
-npx prisma migrate deploy
+node ./node_modules/prisma/build/index.js migrate deploy
 
 echo "[entrypoint] Iniciando aplicación..."
 exec node server.js
